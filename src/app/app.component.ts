6545daf058e59855d8d308bac52ee9e9e1af6ca3
Mike Sharp <msharp23@hotmail.co.uk>
2016-06-26 14:23:02 +0100

    finished exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import {InputComponent} from "./bindings/input.component";
+import {ConfirmComponent} from "./bindings/confirm.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  template: `
+    <div class="container">
+     <my-input (submitted)="onSubmit($event)" [myself]="confirmedMyself"></my-input>
+    </div>
+    <div class="container">
+     <my-confirm (confirmed)="onConfirm($event)" [myself]="myself"></my-confirm>
+    </div>
+  `,
+  styleUrls: ['app.component.css'],
+  directives: [InputComponent, ConfirmComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  myself = {
+    name: '', age: ''
+  };
+
+  confirmedMyself = {
+    name: '', age: ''
+  };
+
+  onSubmit(myself: {name: string, age:string}){
+    this.myself = {name:myself.name, age: myself.age};
+
+  }
+
+  onConfirm(myself: {name: string, age:string}){
+    this.confirmedMyself = {name:myself.name, age: myself.age};
+  }
 }
